0502d15c1f87db461826244e884bc23e4543a7c5
Aloys BERNARD <aloys.bernard@artefact.com>
2026-02-05 15:33:30 +0100

    feat: Add Pierre Feuille Ciseau & Spinner

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import streamlit as st
 import plotly.express as px
+import time 
+import random
 
 @st.cache_data()
 def load_data():
+    time.sleep(1.5)
     df = px.data.iris()
     return df
 
-df = load_data()
+with st.spinner():
+    df = load_data()
 
 col_x = st.select_slider("Choisis la colomne à plot"
                  ,options=["sepal_width", "sepal_length","petal_width", "petal_length",])
@@ -23,3 +27,23 @@ st.title("Hello World")
 
 if st.button("Click me !") : 
     st.balloons()
+    
+    
+st.subheader("Pierre Feuille Ciseaux")
+
+choices = ["Pierre", "Feuille", "Ciseaux"]
+player_choice = st.radio("Choisissez:", choices)
+
+if st.button("Jouer"):
+    computer_choice = random.choice(choices)
+    st.write(f"Vous avez choisi: {player_choice}")
+    st.write(f"L'ordinateur a choisi: {computer_choice}")
+    
+    if player_choice == computer_choice:
+        st.info("Égalité!")
+    elif (player_choice == "Pierre" and computer_choice == "Ciseaux") or \
+            (player_choice == "Feuille" and computer_choice == "Pierre") or \
+            (player_choice == "Ciseaux" and computer_choice == "Feuille"):
+        st.success("Vous avez gagné! 🎉")
+    else:
+        st.error("L'ordinateur a gagné!")
